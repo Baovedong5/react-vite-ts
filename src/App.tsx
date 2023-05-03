94394cedccdf40a8fb461680e0c56338aa42eac0
Phuong <lephuong10142002@gmail.com>
2023-05-03 15:26:46 +0700

    Example basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import Video9 from "./examples/Video9";
+import Video10 from "./examples/Video10/Video10";
+import Video13 from "./examples/Video13";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -26,10 +30,12 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {/* <Video9 /> */}
+        <Video10 defaultName={"Phuong"} defaultAge={21} />
+        {/* <Video13 /> */}
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/examples/Video7.tsx
@@ -0,0 +1,6 @@
+const Video7 = () => {
+  const name = "Phuong";
+  return <h1>Hello World</h1>;
+};
+
+export default Video7;
